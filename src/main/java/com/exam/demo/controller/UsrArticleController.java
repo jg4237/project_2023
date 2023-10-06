bbb56d5e9dfa6685076856a04539527500ab6a9a
jg4237 <fe071634@gmail.com>
2023-10-06 15:22:14 +0900

    article modify 기능 추가

--- src/main/java/com/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/exam/demo/controller/UsrArticleController.java
@@ -71,6 +71,27 @@ public class UsrArticleController {
 		return id + "번 게시물이 삭제되었습니다.";
 	}
 
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if ( article == null ) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id, title, body);
+		return id + "번 게시물이 수정되었습니다.";
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+		
+	}
+
 	private void deleteArticle(int id) {
 		Article article = getArticle(id);
 		
