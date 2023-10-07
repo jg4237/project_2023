b06e6a9ef6871adf41d3d56c450fcfea42f5e4c5
jg4237 <fe071634@gmail.com>
2023-10-07 13:38:40 +0900

    articleService 추가

--- src/main/java/com/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/exam/demo/controller/UsrArticleController.java
@@ -26,8 +26,8 @@ public class UsrArticleController {
 	//
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + 1;
-			String body = "내용" + 1;
+			String title = "제목" + i;
+			String body = "내용" + i;
 	
 			writeArticle(title, body);
 		}
---
+++ src/main/java/com/exam/demo/service/ArticleService.java
@@ -0,0 +1,5 @@
+package com.exam.demo.service;
+
+public class ArticleService {
+
+}
